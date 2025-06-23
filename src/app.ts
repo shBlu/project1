5a84f061f8fb17bc5e793397de6436eebbd8995d
shani <shani@blu.gmail.com>
2025-06-23 14:40:57 +0300

    change now

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,9 @@
 import express from 'express';
 
 const app = express();
-const port =  3000;
 
-app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, TypeScript Express App!');
 });
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,5 @@
-import  express from 'express';
-
-const app = express();
+import app from "./app";
 const PORT =  3000;
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello from TypeScript server!');
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
